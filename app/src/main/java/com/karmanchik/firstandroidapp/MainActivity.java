bea37f3541b61b5c196fd5a91e14d70e3b93da19
arseniy <arseniy.karmanov@mail.ru>
2020-09-29 09:51:07 +0900

    project completed

--- app/src/main/java/com/karmanchik/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/karmanchik/firstandroidapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.karmanchik.firstandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void bntConvertClick(View view) {
+        EditText ptFieldId = findViewById(R.id.ptFieldId);
+        TextView resultText = findViewById(R.id.resultText);
+
+        String text = ptFieldId.getText().toString();
+        if(!text.equals("")) {
+            float number = (float) (Float.parseFloat(text) * 1.6);
+            resultText.setText("В "+text+" милях = "+number+" км.");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
